c736a04b837f46978f9d9124509e87e3d8dde55e
Kesaba Chandra Barik <166729378+KESABA-BARIK@users.noreply.github.com>
2025-03-17 19:39:04 +0530

    Update 2206. Divide Array Into Equal Pairs.cpp

--- 2206. Divide Array Into Equal Pairs.cpp
+++ 2206. Divide Array Into Equal Pairs.cpp
@@ -10,3 +10,15 @@ public:
         return s.empty();
     }
 };
+
+//Alternative
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i+=2){
+            if(nums[i] != nums[i+1])return false;
+        }
+        return true;
+    }
+};
