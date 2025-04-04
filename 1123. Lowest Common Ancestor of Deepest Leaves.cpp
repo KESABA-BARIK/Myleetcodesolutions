50ca49845aba5d3fd4b60fb4f00a8421efa8e72b
Kesaba Chandra Barik <166729378+KESABA-BARIK@users.noreply.github.com>
2025-04-04 10:20:40 +0530

    Create 1123. Lowest Common Ancestor of Deepest Leaves.cpp

---
+++ 1123. Lowest Common Ancestor of Deepest Leaves.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int depth(TreeNode* root){
+    if(!root)return 0;
+    return max(depth(root->left), depth(root->right))+1;
+}
+TreeNode* dfs(TreeNode* root, int depth, int l){
+    if(!root)return nullptr;
+    if(l+1 == depth)return root;
+    TreeNode* left = dfs(root->left, depth, l+1);
+    TreeNode* right = dfs(root->right, depth, l+1);
+    if(left && right)return root;
+    return right ? right:left;
+
+}
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        int d = depth(root);
+        return dfs(root, d, 0);
+    }
+};
